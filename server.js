4e977b9f7bc17a88f16bb7ccf150d70bd54e0c21
jdmorales <jdmg44@hotmail.com>
2016-06-11 06:13:05 +0000

    step_2-> reading_file

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const http = require('http');
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 
 const hostname = '0.0.0.0';
 const port = 8000;
 
 const server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    var pathname = url.parse(req.url).pathname;
+    console.log("-GET [" + pathname + "]");
+    var pathRelative= path.join(process.cwd(),'/appServer/templates/index.html');
+    fs.readFile(pathRelative, function(error, data){
+        if(!error){
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data, "utf8");
+            res.end();
+        }
+    });
 });
 
 server.listen(port, hostname, function() {
